88a99cee586be8fb311a35864f4c8e48d97e2dfd
Brian Chiang <brianchiang1988@icloud.com>
2019-12-05 23:52:13 +0800

    Add Level Order Traversal

---
+++ Data Structures/Tree/Level Order Traversal/level_order_traversal.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    #Write your code here
+
+    level_order_queue = []
+
+    level_order_queue.append( root )
+
+    while len(level_order_queue) != 0:
+
+        upper_node = level_order_queue.pop( 0 )
+
+        print( upper_node.info, end = ' ')
+
+        if upper_node.left is not None:
+            level_order_queue.append( upper_node.left )
+        
+        if upper_node.right is not None:
+            level_order_queue.append( upper_node.right )
+
+    return 
+
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
