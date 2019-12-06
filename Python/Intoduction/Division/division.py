41063aef7c89153d8ee120d9dbd98eb2da34facc
Brian Chiang <brianchiang1988@icloud.com>
2019-12-06 19:58:24 +0800

    Add Division

---
+++ Python/Intoduction/Division/division.py
@@ -0,0 +1,13 @@
+def math_division(a, b):
+
+    print( a// b )
+    print( a/ b )
+
+    return
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    math_division(a, b)
