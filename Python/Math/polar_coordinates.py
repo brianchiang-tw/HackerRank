befdce00829e82e2659a579320c2fbc90b34060c
Brian Chiang <brianchiang1988@icloud.com>
2019-12-07 16:29:15 +0800

    Add Polar Coordinates

---
+++ Python/Math/polar_coordinates.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from cmath import polar
+
+if __name__ == '__main__':
+
+    r, phi = polar( complex( input() ) )
+
+    print( r )
+    print( phi)
+
+
+
