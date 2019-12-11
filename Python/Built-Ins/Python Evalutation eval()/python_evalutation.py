aaa19735bdec2e9afe5fb80aa91700547eb14930
Brian Chiang <brianchiang1988@icloud.com>
2019-12-11 20:12:19 +0800

    Add Python Evaluation eval()

---
+++ Python/Built-Ins/Python Evalutation eval()/python_evalutation.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+
+    expression = str( input() )
+
+    # evaluate string given by input
+    eval(expression)
+   
