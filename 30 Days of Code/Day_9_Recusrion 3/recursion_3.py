67795130e2fc73d46ce47f83ee39c4089b2eea4f
Brian Chiang <brianchiang1988@icloud.com>
2019-12-11 20:09:46 +0800

    Add Day_9 Recursion_3

---
+++ 30 Days of Code/Day_9_Recusrion 3/recursion_3.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n == 0 or n == 1:
+        # Base case:
+        return 1
+    
+    else:
+        # Inductive step
+        return n * factorial(n-1)
+
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
