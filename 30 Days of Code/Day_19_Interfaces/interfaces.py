714a371aafaed73400dc0040b10eb1ab2de02886
Brian Chiang <brianchiang1988@icloud.com>
2019-12-21 11:52:11 +0800

    Add Day_19: Interfaces

---
+++ 30 Days of Code/Day_19_Interfaces/interfaces.py
@@ -0,0 +1,23 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+
+        summation = 0
+
+        for i in range(1, (n + 1)):
+
+            if n % i == 0:
+                #print(" i is divisor {}".format(i) )
+                summation += i
+
+        return summation
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
