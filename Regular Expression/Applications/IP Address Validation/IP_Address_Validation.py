9fe393b019781c335be79ef5940741f84fc7f414
Brian Chiang <brianchiang1988@icloud.com>
2019-12-21 14:03:09 +0800

    Add IP Address Validation

---
+++ Regular Expression/Applications/IP Address Validation/IP_Address_Validation.py
@@ -0,0 +1,51 @@
+import re
+
+regex_ipv4 = r'^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$'
+
+pattern_ipv4 = re.compile(regex_ipv4)
+
+regex_ipv6 = r'^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$'
+
+pattern_ipv6 = re.compile(regex_ipv6)
+
+
+def validate_ipv4(s):
+
+    result = re.match( pattern_ipv4, s )
+
+    if result is not None:
+        return True
+    else:
+        return False
+
+
+
+def validate_ipv6(s):
+
+    result = re.match( pattern_ipv6, s )
+
+    if result is not None:
+        return True
+    else:
+        return False
+
+
+
+if __name__ == '__main__':
+
+    n = int( input() )
+
+    for _ in range(n):
+
+        input_str = input()
+
+        if validate_ipv4(input_str):
+            print("IPv4")
+        
+        elif validate_ipv6(input_str):
+            print("IPv6")
+
+        else:
+            print("Neither")
+
+    
---
+++ Regular Expression/Applications/IP Address Validation/IP_Address_Validation_description.md
@@ -0,0 +1,1 @@
+https://www.hackerrank.com/challenges/ip-address-validation/problem
