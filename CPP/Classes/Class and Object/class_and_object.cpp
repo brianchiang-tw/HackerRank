25278a19b44f286ba18b1e527c16f4a504105ad1
Brian Chiang <brianchiang1988@icloud.com>
2019-12-25 21:17:29 +0800

    Add Class and Object

---
+++ CPP/Classes/Class and Object/class_and_object.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+# include <numeric>
+
+// Write your Student class here
+class Student
+{
+    private:
+        vector<int> scores;
+
+    public:
+        void input()
+        {
+            for( int i = 0 ; i < 5 ; i++)
+            {
+                int s = 0;
+                cin >> s;
+                scores.push_back( s );
+
+            }
+        }
+
+        int calculateTotalScore()
+        {
+            return std::accumulate( scores.begin(), scores.end(), 0 );
+        }
+};
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
