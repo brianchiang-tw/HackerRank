8d6c4df67ef4d9032394b1bac4e09f4d8f088fe1
Brian Chiang <brianchiang1988@icloud.com>
2019-12-25 17:37:21 +0800

    Add Input and Output

---
+++ CPP/Introduction/Input and Output/Input and Output_description.md
@@ -0,0 +1,1 @@
+https://www.hackerrank.com/challenges/cpp-input-and-output/problem
---
+++ CPP/Introduction/Input and Output/input_and_output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    int x=0, y=0, z = 0;
+
+    cin >> x >> y >> z ;
+    cout << (x+y+z);
+    return 0;
+}
