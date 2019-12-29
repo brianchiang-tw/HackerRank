beb92e65d0cdf8d4632766a5f1612b82781ad964
Brian Chiang <brianchiang1988@icloud.com>
2019-12-29 12:09:30 +0800

    Add Integers Come In All Size

---
+++ Python/Math/Integers Come In All Size/integers_come_in_all_size.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a, b, c, d = int(input()), int(input()),int(input()),int(input())
+
+result = int( a**b + c ** d )
+print( result )
