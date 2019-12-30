c44797593deb4c1cc59ed03c5f0e484d583ecff1
Brian Chiang <brianchiang1988@icloud.com>
2019-12-30 15:59:09 +0800

    Add Power as well as Mod Power

---
+++ Python/Math/Power_Mod Power/power_mod_power.py
@@ -0,0 +1,14 @@
+import math
+
+if __name__ == '__main__':
+
+
+
+    a, b, m =  [ int( input() ) for i in range(3) ]
+
+    # Task_#1
+    print( pow(a, b) )
+
+    # Task_#2
+    print( pow(a, b, m) )
+
